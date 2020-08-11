dcf117b93a539560b97aa6090afbdeb7e2a7c51f
shahriar <cse.shahriar.hosen@gmail.com>
2020-08-11 14:26:58 +0600

    Team section

--- js/script.js
+++ js/script.js
@@ -5,3 +5,22 @@ $(window).on('load', function() {
     $("#status").fadeOut();  
     $("#preloader").delay(350).fadeOut('slow'); 
 });
+
+
+/* =================================
+    Team carual
+===================================*/ 
+ $(function() {
+
+    $("#team-members").owlCarousel({
+        items:2,
+        autoplay:true,
+        smartSpeed: 700, 
+        loop:true,
+        autoplayHoverPause:true,    
+        nav:true,
+        navText: ['<i class="fa fa-angle-left"></i>',  '<i class="fa fa-angle-right"></i>'],
+        dots:false,
+    });
+
+});
