9d0606b70a1c2edf990869a2498d4627e458494a
cseshahriar <cse.shahriar.hosen@gmail.com>
2020-09-09 23:57:59 +0600

    Small device responsive css

--- js/script.js
+++ js/script.js
@@ -20,6 +20,20 @@ $(function () {
 		nav: true,
 		navText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>'],
 		dots: false,
+		responsive : {
+			// breakpoint from 0 up
+			0 : {
+				items: 1
+			},
+			// breakpoint from 480 up
+			480 : {
+				items: 2
+			},
+			// breakpoint from 768 up
+			768 : {
+				items: 3
+			} 
+		}
 	});
 
 
