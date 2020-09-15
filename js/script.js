c0b47a7d551a7c712c1e70348d05416b632d371d
cseshahriar <cse.shahriar.hosen@gmail.com>
2020-09-16 01:12:41 +0600

    fontawesome and back to top btn fixed

--- js/script.js
+++ js/script.js
@@ -21,20 +21,14 @@ $(function () {
 		navText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>'],
 		dots: false,
 		responsive : {
-			// breakpoint from 0 up
 			0 : {
 				items: 1
 			},
-			// breakpoint from 480 up
 			480 : {
 				items: 2
-			},
-			// breakpoint from 768 up
-			768 : {
-				items: 3
-			} 
+			}
 		}
-	});
+	}); 
 
 
 	$('#progress-elements').waypoint(function (direction) {
